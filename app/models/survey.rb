038b1487fe5e93c327cb291dffb4520eb674bfa1
Débora Martins <dehvmartins@gmail.com>
2016-03-01 12:04:14 -0300

    validation email logic

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,8 @@ class Survey < ActiveRecord::Base
   has_many :questions, dependent: :destroy
 
   enum status: [ :open, :closed ]
+
+  def valid_emails(emails)
+    emails.select {|email| !!(email =~ /@/)}
+  end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe Survey, type: :model do
     expect(survey).to be_valid
   end
 
+  context "validations" do
+    it 'has valid emails' do
+      emails = (1..5).map {"#{('A'..'z').to_a.shuffle[0,8].join}@#{('A'..'z').to_a.shuffle[0,8].join}"}
+      expect(survey.valid_emails(emails).size).to eq(emails.size)
+    end
+
+    it 'has invalid emails' do
+      emails = (1..5).map {"#{('A'..'z').to_a.shuffle[0,8].join}#{('A'..'z').to_a.shuffle[0,8].join}"}
+      expect(survey.valid_emails(emails).size).to_not eq(emails.size)
+    end
+  end
+
+
 end
