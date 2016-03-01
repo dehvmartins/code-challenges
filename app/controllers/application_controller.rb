1b44a63a8f47bc62012d598ce0bad68a3e6f4cc1
Débora Martins <dehvmartins@gmail.com>
2016-03-01 11:38:12 -0300

    Authentication logic added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    Survey.where("? = ANY(token)", params[:token]).exists? if params[:token]
+  end
+
+  def render_unauthorized
+    render json: 'Você não está autorizado a acessar essa página', status: 401
+  end
 end
